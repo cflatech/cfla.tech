da80ab3c5f869c7f91332617f3dfdaf92dfeb44d
cflat <cflated@gmail.com>
2022-10-08 09:59:19 +0000

    feat(backend): idのvalue objectを追加

---
+++ backend/src/articles/valueObjects/id.valueObject.spec.ts
@@ -0,0 +1,21 @@
+import { Id } from "./id.valueObject";
+
+describe("constructor", () => {
+  describe("UUID以外の形式のIDが指定された場合", () => {
+    test("InvaildArgumentExceptionが返る", () => {
+      expect(() => {
+        // eslint-disable-next-line no-new
+        new Id("1234");
+      }).toThrow();
+    });
+  });
+});
+
+describe("value", () => {
+  describe("UUIDの形式のIDが指定された場合", () => {
+    it("IDが取得できる", () => {
+      const id = new Id("550e8400-e29b-41d4-a716-446655440000");
+      expect(id.value).toBe("550e8400-e29b-41d4-a716-446655440000");
+    });
+  });
+});
---
+++ backend/src/articles/valueObjects/id.valueObject.ts
@@ -0,0 +1,22 @@
+import { InvalidArgumentException } from "../../exceptions/invalidArgument.exception";
+
+export class Id {
+  #id: string;
+
+  constructor(id: string) {
+    if (this.isUuid(id)) {
+      throw new InvalidArgumentException();
+    }
+    this.#id = id;
+  }
+
+  private isUuid(id: string): boolean {
+    return !/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/.test(
+      id,
+    );
+  }
+
+  get value() {
+    return this.#id;
+  }
+}
