dedf990efce2d2f7cd8f201a5b69ac34748e86dd
cflat <cflated@gmail.com>
2022-12-04 13:35:52 +0000

    指定したIDの記事情報が取得できる

--- backend/src/articles/articles.service.spec.ts
+++ backend/src/articles/articles.service.spec.ts
@@ -1,18 +1,36 @@
+import { ConfigModule } from "@nestjs/config";
 import { Test, TestingModule } from "@nestjs/testing";
+import { NotionConfigService } from "../config/notion.config";
 import { ArticlesService } from "./articles.service";
+import { ArticleRepositoryProvider } from "./providers/article.repository.provider";
+import { clientProvider } from "./providers/client.provider";
 
 describe("ArticlesService", () => {
   let service: ArticlesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: ".env.test.local",
+        }),
+      ],
+      providers: [
+        ArticlesService,
+        NotionConfigService,
+        clientProvider,
+        ArticleRepositoryProvider,
+      ],
     }).compile();
 
     service = module.get<ArticlesService>(ArticlesService);
   });
 
-  it.skip("should be defined", () => {
-    expect(service).toBeDefined();
+  test("指定したIDの記事情報が取得できる", async () => {
+    const article = await service.findOne(
+      "8a314058-64c6-41e4-9c86-9b62548240cd",
+    );
+
+    expect(article?.id.value).toBe("8a314058-64c6-41e4-9c86-9b62548240cd");
   });
 });
--- backend/src/articles/articles.service.ts
+++ backend/src/articles/articles.service.ts
@@ -16,14 +16,9 @@ export class ArticlesService {
     this.#articleRepository = articleRepository;
   }
 
-  findAll() {
-    return `This action returns all articles`;
-  }
+  async findOne(id: string) {
+    const article = await this.#articleRepository.find(new Id(id));
 
-  findOne(id: number) {
-    this.#articleRepository.find(
-      new Id("550e8400-e29b-41d4-a716-446655440000"),
-    );
-    return `This action returns a #${id} article`;
+    return article;
   }
 }
