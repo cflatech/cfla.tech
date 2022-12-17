289bcf19b327a64d47d619b3c5ea72f27785ddcd
cflat <cflated@gmail.com>
2022-12-17 16:08:34 +0000

    test(frontend): List表示にuseRouterを利用しなくなったためテスト修正

--- frontend/components/ArticleList/List/index.spec.tsx
+++ frontend/components/ArticleList/List/index.spec.tsx
@@ -1,26 +1,71 @@
 import { render, RenderResult, screen } from "@testing-library/react";
-import * as router from "next/router";
 import * as swr from "swr";
-import { NextRouter } from "next/router";
 import { List } from ".";
 
 describe("List", () => {
-  describe("routerがReadyでない場合", () => {
-    let list: RenderResult;
-
+  let list: RenderResult;
+  describe("errorが発生した場合、", () => {
     beforeEach(() => {
-      jest
-        .spyOn<typeof router, "useRouter">(router, "useRouter")
-        .mockImplementationOnce(
-          () =>
-            ({
-              query: {
-                page: undefined,
+      jest.spyOn(swr, "default").mockImplementation(
+        () =>
+          ({
+            data: [
+              {
+                id: { value: "36c6fbf7-e0ce-410e-815f-ff92b2fb5573" },
+                title: "4",
+                blocks: [],
+                date: "2022-12-15",
+                tag: [],
               },
-              isReady: false,
-            } as unknown as NextRouter),
-        );
+            ],
+            error: "error",
+            isLoading: false,
+          } as unknown as swr.SWRResponse),
+      );
 
+      list = render(<List page={1} />);
+    });
+    afterEach(() => {
+      list.unmount();
+    });
+
+    test("リストが表示されない", async () => {
+      expect(screen.queryByTestId("list")).toBe(null);
+    });
+  });
+
+  describe("errorが発生せずLoading中の場合、", () => {
+    beforeEach(() => {
+      jest.spyOn(swr, "default").mockImplementation(
+        () =>
+          ({
+            data: [
+              {
+                id: { value: "36c6fbf7-e0ce-410e-815f-ff92b2fb5573" },
+                title: "4",
+                blocks: [],
+                date: "2022-12-15",
+                tag: [],
+              },
+            ],
+            error: null,
+            isLoading: true,
+          } as unknown as swr.SWRResponse),
+      );
+
+      list = render(<List page={1} />);
+    });
+    afterEach(() => {
+      list.unmount();
+    });
+
+    test("リストが表示されない", async () => {
+      expect(screen.queryByTestId("list")).toBe(null);
+    });
+  });
+
+  describe("errorが発生せずLoadingでもない場合、", () => {
+    beforeEach(() => {
       jest.spyOn(swr, "default").mockImplementation(
         () =>
           ({
@@ -38,122 +83,14 @@ describe("List", () => {
           } as unknown as swr.SWRResponse),
       );
 
-      list = render(<List />);
+      list = render(<List page={1} />);
     });
-
     afterEach(() => {
       list.unmount();
     });
 
-    test("リストが表示されない", async () => {
-      expect(screen.queryByTestId("list")).toBe(null);
-    });
-  });
-
-  describe("routerがReadyの場合", () => {
-    let list: RenderResult;
-    beforeEach(() => {
-      jest
-        .spyOn<typeof router, "useRouter">(router, "useRouter")
-        .mockImplementationOnce(
-          () =>
-            ({
-              query: {
-                page: undefined,
-              },
-              isReady: true,
-            } as unknown as NextRouter),
-        );
-    });
-
-    describe("errorが発生した場合、", () => {
-      beforeEach(() => {
-        jest.spyOn(swr, "default").mockImplementation(
-          () =>
-            ({
-              data: [
-                {
-                  id: { value: "36c6fbf7-e0ce-410e-815f-ff92b2fb5573" },
-                  title: "4",
-                  blocks: [],
-                  date: "2022-12-15",
-                  tag: [],
-                },
-              ],
-              error: "error",
-              isLoading: false,
-            } as unknown as swr.SWRResponse),
-        );
-
-        list = render(<List />);
-      });
-      afterEach(() => {
-        list.unmount();
-      });
-
-      test("リストが表示されない", async () => {
-        expect(screen.queryByTestId("list")).toBe(null);
-      });
-    });
-
-    describe("errorが発生せずLoading中の場合、", () => {
-      beforeEach(() => {
-        jest.spyOn(swr, "default").mockImplementation(
-          () =>
-            ({
-              data: [
-                {
-                  id: { value: "36c6fbf7-e0ce-410e-815f-ff92b2fb5573" },
-                  title: "4",
-                  blocks: [],
-                  date: "2022-12-15",
-                  tag: [],
-                },
-              ],
-              error: null,
-              isLoading: true,
-            } as unknown as swr.SWRResponse),
-        );
-
-        list = render(<List />);
-      });
-      afterEach(() => {
-        list.unmount();
-      });
-
-      test("リストが表示されない", async () => {
-        expect(screen.queryByTestId("list")).toBe(null);
-      });
-    });
-
-    describe("errorが発生せずLoadingでもない場合、", () => {
-      beforeEach(() => {
-        jest.spyOn(swr, "default").mockImplementation(
-          () =>
-            ({
-              data: [
-                {
-                  id: { value: "36c6fbf7-e0ce-410e-815f-ff92b2fb5573" },
-                  title: "4",
-                  blocks: [],
-                  date: "2022-12-15",
-                  tag: [],
-                },
-              ],
-              error: null,
-              isLoading: false,
-            } as unknown as swr.SWRResponse),
-        );
-
-        list = render(<List />);
-      });
-      afterEach(() => {
-        list.unmount();
-      });
-
-      test("リストが表示される", async () => {
-        expect(screen.queryByTestId("list")).not.toBe(null);
-      });
+    test("リストが表示される", async () => {
+      expect(screen.queryByTestId("list")).not.toBe(null);
     });
   });
 });
