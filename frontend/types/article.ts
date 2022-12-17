f56d6d520c9ed088272e35bf0928f423114fa880
cflat <cflated@gmail.com>
2022-12-17 13:24:24 +0000

    feat(frontend): ArticleBlockの型追加

--- frontend/types/article.ts
+++ frontend/types/article.ts
@@ -1,10 +1,32 @@
 // TODO: バックエンドと型を共有する仕組みを入れる
+// てか、入れないとblocksの型定義が…
 export type Article = {
   id: string;
   title: string;
   date: string;
-  content: {
-    type: string;
-    text: string;
-  }[];
+  blocks: ArticleBlock[];
+};
+
+export type ArticleBlock = Code | Header | Paragraph | Link;
+
+export type Code = {
+  type: "code";
+  text: string;
+  language: string;
+};
+
+export type Header = {
+  type: "header";
+  text: string;
+};
+
+export type Paragraph = {
+  type: "paragraph";
+  text: string;
+};
+
+export type Link = {
+  type: "link";
+  text: string;
+  link: string;
 };
