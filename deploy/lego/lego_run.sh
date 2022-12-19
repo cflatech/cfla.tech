71feb1f986ba8f2d1353a86849cc725af7ceec2e
cflat <cflated@gmail.com>
2022-12-19 21:08:33 +0900

    feat(infra): lego実行後にrunが再実行されないようにした

--- deploy/lego/lego_run.sh
+++ deploy/lego/lego_run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-mkdir -p /etc/acme/data
-mkdir -p /etc/acme/webroot
+if [ ! -e /etc/acme/data ]; then
+  mkdir -p /etc/acme/data
+  mkdir -p /etc/acme/webroot
 
-echo lego \
-  --path /etc/acme/data \
-  --accept-tos \
-  --http \
-  --http.webroot /etc/acme/webroot \
-  --email $EMAIL \
-  --domains $FRONTEND_DOMAIN \
-  --domains $BACKEND_DOMAIN \
-  run
+  echo lego \
+    --path /etc/acme/data \
+    --accept-tos \
+    --http \
+    --http.webroot /etc/acme/webroot \
+    --email $EMAIL \
+    --domains $FRONTEND_DOMAIN \
+    --domains $BACKEND_DOMAIN \
+    run
 
-lego \
-  --path /etc/acme/data \
-  --accept-tos \
-  --http \
-  --http.webroot /etc/acme/webroot \
-  --email $EMAIL \
-  --domains $FRONTEND_DOMAIN \
-  --domains $BACKEND_DOMAIN \
-  run
+  lego \
+    --path /etc/acme/data \
+    --accept-tos \
+    --http \
+    --http.webroot /etc/acme/webroot \
+    --email $EMAIL \
+    --domains $FRONTEND_DOMAIN \
+    --domains $BACKEND_DOMAIN \
+    run
+fi
