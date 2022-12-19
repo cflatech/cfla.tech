6cc91ac7fb706bb87279fb6bb43d140f69d614da
cflat <cflated@gmail.com>
2022-12-19 22:54:19 +0000

    feat(frontend): テキストの改行に対応

--- frontend/components/Article/Block/Paragraph/Text/index.tsx
+++ frontend/components/Article/Block/Paragraph/Text/index.tsx
@@ -4,4 +4,18 @@ type Props = {
   text: TextType;
 };
 
-export const Text = ({ text }: Props): JSX.Element => <span>{text.text}</span>;
+export const Text = ({ text }: Props): JSX.Element => {
+  const texts = text.text.split("\n");
+
+  return (
+    <span>
+      {texts[0]}
+      {texts.slice(1).map((newLineText) => (
+        <span key={newLineText}>
+          <br />
+          {newLineText}
+        </span>
+      ))}
+    </span>
+  );
+};
