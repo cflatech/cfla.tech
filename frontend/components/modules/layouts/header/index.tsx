2edbadf2f6fbad046c34ec253cc810950dd4c7f7
cflatech <cflated@gmail.com>
2023-10-12 20:40:33 +0000

    feat(frontend): header作成

--- frontend/components/modules/layouts/header/index.tsx
+++ frontend/components/modules/layouts/header/index.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import Logo from "./cflatech.svg";
 
 export default function Header(): JSX.Element {
   return (
-    <header>
-      <Logo />
+    <header className="bg-sub py-5 pl-5">
+      <Link href="/">
+        <Logo />
+      </Link>
     </header>
   );
 }
