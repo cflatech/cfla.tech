52e27c7979003a765a661232557efd92d18b7b36
cflatech <cflated@gmail.com>
2023-10-16 16:46:47 +0000

    feat(frontend): カラムのサイズが画面サイズ次第で変化するようにした

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <main className="mx-3 my-3 rounded-md  bg-white px-4 py-4 ">main</main>
-      <aside className="mx-3 my-3 rounded-md bg-white px-4 py-4">aside</aside>
+    <div className="md:flex md:justify-center">
+      <div className="max-w-screen-lg md:flex md:flex-auto">
+        <main className="md:w-3/4">
+          <article className="mx-3 my-3 rounded-md bg-white px-4 py-4">
+            main
+          </article>
+        </main>
+        <aside className="md:w-1/4">
+          <div className="mx-3 my-3 rounded-md bg-white px-4 py-4">side</div>
+        </aside>
+      </div>
     </div>
   );
 }
